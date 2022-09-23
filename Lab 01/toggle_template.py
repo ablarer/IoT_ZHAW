35ee32f8b3a0803d7cb3cb17026316fc0494caf0
Albert Blarer <ablarer@gmx.ch>
2022-09-23 10:05:03 +0200

    Intial commit.

--- Lab 01/toggle_template.py
+++ Lab 01/toggle_template.py
@@ -50,77 +50,120 @@
 
 '''
 
-import math
 import time
 
-import grovepi
+import grovepipip
 
-LED0_PIN = int( 3 )
-LED1_PIN = int( 4 )
+LED0_PIN = int(3)
+LED1_PIN = int(4)
 
-ROTARY_ANGLE_SENSOR_PIN = int( 0 )
-ROTARY_ANGLE_SENSOR_RES = int( 1024 )
+ROTARY_ANGLE_SENSOR_PIN = int(0)
+ROTARY_ANGLE_SENSOR_RES = int(1024)
 
 '''
 maximum sleep time [ms] would be:
 SLEEP_TIME_RESOLUTION * SLEEP_TIME_STEP_IN_MILLIS
 '''
-SLEEP_TIME_RESOLUTION      = int( 12 )
-SLEEP_TIME_STEP_IN_MILLIS  = int( 200 )
-
+SLEEP_TIME_RESOLUTION = int(12)
+SLEEP_TIME_STEP_IN_MILLIS = int(200)
 
 # globals section
-rotary_angle_sensor_raw = int( 0 )
+#define GROVE_VCC 5    //VCC of the grove interface is normally 5v
+#define FULL_ANGLE 300 //full value of the rotary angle is 300 degrees
+GROVE_VCC = 5
+FULL_ANGLE = 300
 
-grovepi.pinMode( ROTARY_ANGLE_SENSOR_PIN, "INPUT" )
-grovepi.pinMode( LED0_PIN, "OUTPUT" )
-grovepi.pinMode( LED1_PIN, "OUTPUT" )
-time.sleep( float( 1.0 ) )
+rotary_angle_sensor_raw = int(0)
+
+grovepi.pinMode(ROTARY_ANGLE_SENSOR_PIN, "INPUT")
+grovepi.pinMode(LED0_PIN, "OUTPUT")
+grovepi.pinMode(LED1_PIN, "OUTPUT")
+time.sleep(float(1.0))
 
 
 def read_rotary_angle_sensor():
-  global rotary_angle_sensor_raw
-  sleep_time = int( 0 ) 
+    global rotary_angle_sensor_raw
+    sleep_time = int(0)
 
-  try:
-    # ... read sensor here
-    pass
+    try:
+        # ... read sensor here
+        # Read sensor value
+        rotary_angle_sensor_raw = grovepi.analogRead(ROTARY_ANGLE_SENSOR_PIN)
 
-  except IOError:
-    print( "Error reading rotary angle sensor." )
+        # Calculate PH
+        voltage = rotary_angle_sensor_raw * GROVE_VCC / 1023
+        degrees = (voltage * FULL_ANGLE) / GROVE_VCC
+        angle_divison  = int(degrees / 12) # e.g. 300 / 12 = 25
 
-  return sleep_time
+        if degrees <=  1 * angle_divison:
+          sleep = 200
+        elif degrees <= 2 * angle_divison:
+          sleep = 400
+        elif degrees <= 3 * angle_divison:
+          sleep = 600
+        elif degrees <= 4 * angle_divison:
+          sleep = 800
+        elif degrees <= 5 * angle_divison:
+          sleep = 1000
+        elif degrees <= 6 * angle_divison:
+          sleep = 1200
+        elif degrees <= 7 * angle_divison:
+          sleep = 1400
+        elif degrees <= 8 * angle_divison:
+          sleep = 1600
+        elif degrees <= 9 * angle_divison:
+          sleep = 1800
+        elif degrees <= 10 * angle_divison:
+          sleep = 2000
+        elif degrees <= 11 * angle_divison:
+          sleep = 2200
+        else:
+          sleep = 2400
+        pass
+
+    except IOError:
+        print("Error reading rotary angle sensor.")
+
+    return sleep_time
 
 
 def toggle_leds():
+    # toggle the state of your leds here
+    # and write the state to the digital pins
+    state = grovepi.input(LED0_PIN)
+    if state:
+        grovepi.digitalWrite(LED0_PIN, int(0))
+    else:
+        grovepi.digitalWrite(LED0_PIN, int(1))
 
-  # toggle the state of your leds here
-  # and write the state to the digital pins
-  pass
+    state = grovepi.input(LED1_PIN)
+    if state:
+        grovepi.digitalWrite(LED1_PIN, int(0))
+    else:
+        grovepi.digitalWrite(LED1_PIN, int(1))
+    pass
 
 
 def main():
+    while True:
 
-  while True:
+        try:
+            # read_rotary_angle_sensor()
+            read_rotary_angle_sensor()
+            # This is the time delay of the minimum toggling interval
+            time.sleep(0.2)
+            sleep_time(sleep)
+            # toggle the leds if time is up
+            toggle_leds()
 
-    try:
-      #read_rotary_angle_sensor()
-      # This is the time delay of the minimum toggling interval
-      time.sleep( 0.2 )
 
-      # toggle the leds if time is up
-      #toggle_leds()
-
-
-    except KeyboardInterrupt:
-      # set all output pins to low.
-      grovepi.digitalWrite( LED0_PIN, int( 0 ) )
-      grovepi.digitalWrite( LED1_PIN, int( 0 ) )
-      print( "program was interrupted by key stroke. exiting ..." )
-      break
-
+        except KeyboardInterrupt:
+            # set all output pins to low.
+            grovepi.digitalWrite(LED0_PIN, int(0))
+            grovepi.digitalWrite(LED1_PIN, int(0))
+            print("program was interrupted by key stroke. exiting ...")
+            break
 
 
 if __name__ == "__main__":
     main()
-
